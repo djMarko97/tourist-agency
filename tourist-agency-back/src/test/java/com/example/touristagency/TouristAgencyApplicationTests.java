fa8dc3eb642d661ad5e12ce59b8df61dbcdab3c1
MARKO DJORDJEVIC <djordjevic.marko373@gmail.com>
2021-09-03 18:26:16 +0200

    Initial Commit

---
+++ tourist-agency-back/src/test/java/com/example/touristagency/TouristAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.touristagency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristAgencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
