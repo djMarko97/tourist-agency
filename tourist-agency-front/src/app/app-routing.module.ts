feb688f650f24c3713d43a7efb29f298af27f04b
Vuk <djordjevicvuk123@gmail.com>
2021-09-19 12:44:46 +0200

    Adds basic-layout

--- tourist-agency-front/src/app/app-routing.module.ts
+++ tourist-agency-front/src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { HeaderComponent } from './common/header/header.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { AuthGuard } from './guards/auth.guard';
+import { BasicLayoutComponent } from './layouts/basic-layout/basic-layout.component';
 const routes: Routes = [
   {path: '', component: HeaderComponent, children:[
 
   ]},
   {
-    path: '', children:[
+    path: '', component: BasicLayoutComponent, children:[
       {path: 'login', canActivate:[AuthGuard], component: LoginComponent},
       {path:'register', component: RegisterComponent}
     ]
